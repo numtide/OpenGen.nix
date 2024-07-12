4fc0f17b391ec1d8b6479a204c91d95752b003d0
Samuel Rounce <me@samuelrounce.co.uk>
2024-07-12 11:23:19 +0100

    fix(dm-tree): Use fetchPypi and enable Darwin platforms

--- pkgs/python-modules/dm-tree/default.nix
+++ pkgs/python-modules/dm-tree/default.nix
@@ -1,82 +1,76 @@
-{
-  abseil-cpp,
-  absl-py,
-  attrs,
-  autoPatchelfHook,
-  buildPythonPackage,
-  cmake,
-  fetchFromGitHub,
-  fetchurl,
-  lib,
-  numpy,
-  pybind11,
-  six,
-  stdenv,
-  wrapt,
+
+{ autoPatchelfHook
+, buildPythonPackage
+, fetchPypi
+, python
+, isPy311
+, lib
+, stdenv
 }:
 let
-  srcs = {
-    "aarch64-linux" = fetchurl {
-      url = "https://files.pythonhosted.org/packages/fe/89/386332bbd7567c4ccc13aa2e58f733237503fc75fb389955d3b06b9fb967/dm_tree-0.1.8-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl";
-      hash = "sha256-FgfOSapC8BDR5eYW2SzomdZoNdTYvqSWeVgkNShVFd4=";
-    };
-
-    "x86_64-linux" = fetchurl {
-      url = "https://files.pythonhosted.org/packages/4a/27/c5e3580a952a07e5a1428ae952874796870dc8db789f3d774e886160a9f4/dm_tree-0.1.8-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
+  prebuiltWheels = {
+    "3.11-x86_64-linux" = {
+      platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
+      dist = "cp311";
       hash = "sha256-g7d2TeDYVTOKvvxuPun+QNMBZoMQqjuuo/d4/wUfQ5M=";
     };
-
-    "aarch64-darwin" = fetchurl {
-      url = "https://files.pythonhosted.org/packages/e2/64/901b324804793743f0fdc9e47db893bf0ded9e074850fab2440af330fe83/dm_tree-0.1.8-cp311-cp311-macosx_10_9_universal2.whl";
-      hash = "sha256-rRbOupClbsR89FshhW0UlirDFHh5de94bvtebpynXsc=";
+    "3.11-aarch64-linux" = {
+      platform = "manylinux_2_17_aarch64.manylinux2014_aarch64";
+      dist = "cp311";
+      hash = "sha256-FgfOSapC8BDR5eYW2SzomdZoNdTYvqSWeVgkNShVFd4=";
     };
-
-    "x86_64-darwin" = fetchurl {
-      url = "https://files.pythonhosted.org/packages/e2/64/901b324804793743f0fdc9e47db893bf0ded9e074850fab2440af330fe83/dm_tree-0.1.8-cp311-cp311-macosx_10_9_universal2.whl";
-      hash = "sha256-rRbOupClbsR89FshhW0UlirDFHh5de94bvtebpynXsc=";
+    "3.11-x86_64-darwin" = {
+      platform = "macosx_10_9_x86_64";
+      dist = "cp311";
+      hash = "sha256-gDv8U7Rln0R6xpTb0EI1+Upz73wf0eDffISsQeC8ljs=";
+    };
+    "3.11-aarch64-darwin" = {
+      platform = "macosx_11_0_arm64";
+      dist = "cp311";
+      hash = "sha256-N4zIrZPF/jWQ9AWjCZgHIfAhx5DKG9+bFbsdWdrsV/U=";
     };
   };
+  
+  pyVersion = lib.versions.majorMinor python.version;
+  srcInputs =
+    prebuiltWheels."${pyVersion}-${stdenv.system}"
+      or (throw "dm-tree for Python version '${pyVersion}' is not supported on '${stdenv.system}'");
 in
-
 buildPythonPackage rec {
   pname = "dm-tree";
   version = "0.1.8";
   format = "wheel";
 
-  src = srcs.${stdenv.system} or (throw "system ${stdenv.system} not supported");
+  disabled = !isPy311;
 
-  nativeBuildInputs = [
+  src = fetchPypi {
+    inherit version;
+    inherit (srcInputs) platform dist hash;
+
+    pname = (builtins.replaceStrings [ "-" ] [ "_" ] pname);
+
+    python = srcInputs.dist;
+    abi = srcInputs.dist;
+
+    format = "wheel";
+  };
+
+  nativeBuildInputs = (lib.optionals stdenv.isLinux [
     autoPatchelfHook
-    # pybind11
-  ];
+  ]);
 
-  buildInputs = [
-    # abseil-cpp
-    # pybind11
-    stdenv.cc.cc
-  ];
-
-  propagatedBuildInputs = [ six ];
-
-  # nativeCheckInputs = [
-  #   absl-py
-  #   attrs
-  #   numpy
-  #   wrapt
-  # ];
+  # Dynamic link dependencies
+  buildInputs = [ stdenv.cc.cc ];
 
   pythonImportsCheck = [ "tree" ];
 
-  passthru.srcs = srcs;
-
   meta = with lib; {
-    broken = stdenv.isDarwin;
-    description = "Tree is a library for working with nested data structures";
-    homepage = "https://github.com/deepmind/tree";
-    license = licenses.asl20;
-    maintainers = with maintainers; [
-      samuela
-      ndl
+    description = "Tree is a library for working with nested data structures.";
+    homepage    = "https://github.com/deepmind/tree";
+    license     = licenses.asl20;
+    platforms = [
+      "aarch64-linux" "x86_64-linux"
+      "aarch64-darwin" "x86_64-darwin"
     ];
   };
 }
