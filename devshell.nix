7343a8bbed11a83b5c6314370f231217dd48486c
Samuel Rounce <me@samuelrounce.co.uk>
2024-07-12 10:45:47 +0100

    fix(devshell): Execute CUDA-specific shellhook code only if CUDA is supported

--- devshell.nix
+++ devshell.nix
@@ -1,37 +1,36 @@
 { ... }: {
-  perSystem =
-    {
-      config,
-      self',
-      pkgs,
-      system,
-      ...
-    }:
-    let
-      devshellPython = (
-        self'.legacyPackages.python3Packages.python.withPackages (p: [
-          self'.legacyPackages.python3Packages.bayes3d
-          self'.legacyPackages.python3Packages.jax
-          p.jupyter
-          p.scipy
-        ])
-      );
-    in
-    {
-      devShells.default = pkgs.mkShell {
-        packages = [
-          self'.legacyPackages.python3Packages.python-lsp-server
-          devshellPython
-        ];
+  perSystem = { self', pkgs, ... }:
+  let
+    inherit (pkgs) lib config;
 
-        shellHook = ''
-          export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-          export EXTRA_CCFLAGS="-I/usr/include"
-          export CUDA_PATH=${pkgs.cudatoolkit_11}
-          export B3D_ASSET_PATH="${self'.packages.bayes3d.src}/assets"
+    devshellPython = (
+      self'.legacyPackages.python3Packages.python.withPackages (p: [
+        self'.legacyPackages.python3Packages.bayes3d
+        self'.legacyPackages.python3Packages.jax
+        p.jupyter
+        p.scipy
+      ])
+    );
 
-          jupyter notebook
-        '';
-      };
+    cudaShellHook = ''
+      export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+      export CUDA_PATH=${pkgs.cudatoolkit_11}
+    '';
+  in
+  {
+    devShells.default = pkgs.mkShell {
+      packages = [
+        self'.legacyPackages.python3Packages.python-lsp-server
+        devshellPython
+      ];
+
+      shellHook = ''
+        ${lib.optionalString config.cudaSupport cudaShellHook}
+        export EXTRA_CCFLAGS="-I/usr/include"
+        export B3D_ASSET_PATH="${self'.packages.bayes3d.src}/assets"
+
+        jupyter notebook
+      '';
     };
-  }
+  };
+}
